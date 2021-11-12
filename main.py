21caee61d8806cdfeac3d1f0940831415bb6f18e
Oliwn <om.strauss@outlook.com>
2021-11-12 19:27:33 +0100

    Changing input.csv and improving reading from csv

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 # Clustering
 #oli stinkt
 
+from scipy.spatial.distance import cityblock
 import pandas as pd
 
-data = pd.read_csv('input.csv', sep= ";", header = None)
+data = pd.read_csv('input.csv', sep=";", header=None, skiprows=2, dtype=float)
 print (data)
+
+print(cityblock(data.iloc[0], data.iloc[1]))
