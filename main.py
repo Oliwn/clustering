0926171dec4c4cfd3f44fadf9f1b8a11dfc8d2f0
Johlf <antiga.gralf@hotmail.com>
2021-11-13 18:17:41 +0100

    list with kpoints lists full with distances calculated with each kpoint list with all points in current clusters

--- main.py
+++ main.py
@@ -33,3 +33,48 @@ print(centroids)
 print(centroids[0])
 print(centroids[1])
 print(centroids[2])
+#creates a list with all calculated distances between each point and each kpoint
+cendist1=[]
+cendist2=[]
+cendist3=[]
+
+for points in range(len(df)):
+
+    dist1=cityblock(centroids[0],df.iloc[points])
+    dist2=cityblock(centroids[1],df.iloc[points])
+    dist3= cityblock(centroids[2],df.iloc[points])
+
+    cendist1.append(dist1)
+    cendist2.append(dist2)
+    cendist3.append(dist3)
+print()
+print(cendist1)
+print(cendist2)
+print(cendist3)
+print()
+print(len(cendist1))
+print(len(cendist2))
+print(len(cendist3))
+
+#checks which kpoint is closer to each data entry and saves the data in the kpoint list to which its closer to
+for temp in range(len(cendist1)):
+    print(temp)
+    if cendist1[temp] < cendist2[temp] and cendist1[temp] < cendist3[temp]:
+        cendist2.pop(temp)
+        cendist3.pop(temp)
+    elif cendist2[temp] < cendist1[temp] and cendist2[temp] < cendist3[temp]:
+        cendist1.pop(temp)
+        cendist3.pop(temp)
+    elif cendist3[temp] < cendist2[temp] and cendist3[temp] < cendist1[temp]:
+        cendist2.pop(temp)
+        cendist1.pop(temp)
+
+print("new list")
+print(cendist1)
+print(cendist2)
+print(cendist3)
+#next step: calculate the average of each list and define them as new k-points
+#loop the whole thing
+#creat export file with the numbers
+#for the export file: important to implement a counter that counts how often a dataset got a signed to a cluster
+#for better reference check output.csv
